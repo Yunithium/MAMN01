ec2373536c29e364df1cf657798b71b81b03db04
OguzTaskin <KingDrowser@gmail.com>
2013-12-12 21:17:11 +0100

    RollingStone

---
+++ RollingStone/src/com/example/advancement/CustomActivity.java
@@ -0,0 +1,10 @@
+package com.example.advancement;
+
+import android.content.Intent;
+import android.os.Vibrator;
+
+public interface CustomActivity {
+	public void changeActivity();
+	public Vibrator getVibrator();
+	public void sendOrderedBroadcast(Intent downIntent, String s);
+}
